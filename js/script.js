4a840820f007d0d7f02efd4d916b0aaf2d614668
dmitrii <hdmail2022@gmail.com>
2023-05-01 22:51:33 +0400

    add mobile menu, add adaptive

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    const hamburger = document.querySelector('#hamburger');
+    const menu = document.querySelector('#menu');
+    const liItem = document.querySelectorAll('.menu_item');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    })
+
+    liItem.forEach(el => {
+        el.addEventListener('click', () => {
+            hamburger.classList.toggle('amburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+});
